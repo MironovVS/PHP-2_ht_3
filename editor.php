d99fc15b689077d70b300b850070e08e78ec6bd3
MironovVS <MAdd2062@gmail.com>
2015-11-26 00:16:39 +0400

    Модифицировал консоль редактора через вложенные шаблоны

--- editor.php
+++ editor.php
@@ -9,8 +9,11 @@ $articles_all = articles_all();
 // Заголовок страницы
 $title="Консоль редактора";
 
-// Заготовка страницы
-$content='theme/editor.php';
+// Внутренний шаблон.
+$content = template('theme/template/v_editor.php', array('articles_all'=>$articles_all));
+
+// Внешний шаблон.
+$page = template('theme/v_main.php', array('title' => $title, 'content' => $content));
 
 // Вывод HTML
-include('Pattern/pattern-main.php');
+echo $page;
